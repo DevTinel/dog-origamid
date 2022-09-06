772224f8872f5c37038f11ebd5f07ed8cd5d328b
DevTinel <davidaugustolol36@gmail.com>
2022-09-05 23:37:57 -0300

    commentarios

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { UserContext } from '../../UserContext';
 import { PhotoCommentsForm } from './PhotoCommentsForm';
 import styles from './PhotoComments.module.css';
+import { useEffect } from 'react';
 
 export const PhotoComments = (props) => {
   const [comments, setComments] = React.useState(() => props.comments);
   const { login } = React.useContext(UserContext);
+  const commentsSections = useRef(null);
+
+  useEffect(() => {
+    commentsSections.current.scrollTop = commentsSections.current.scrollHeight;
+  }, [comments]);
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul ref={commentsSections} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
